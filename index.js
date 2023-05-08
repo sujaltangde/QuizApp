da8d4ed0eb5d40a42d157ace6ab93361850326d8
Sujal Tangde <sujaltagade01@gmail.com>
2023-05-08 23:59:28 +0530

    first commit

--- index.js
+++ index.js
@@ -85,8 +85,8 @@ const questions = [
     {
         question: 'What is the most widely used programming language?',
         answers: [
-            {text: 'JavaScript', correct: true},
             {text: 'Python', correct: false},
+            {text: 'JavaScript', correct: true},
             {text: 'Java', correct: false},
             {text: 'C++', correct: false}
         ]
@@ -94,19 +94,19 @@ const questions = [
     {
         question: 'What is the file extension for JavaScript files?',
         answers: [
-            {text: '.js', correct: true},
             {text: '.html', correct: false},
             {text: '.css', correct: false},
+            {text: '.js', correct: true},
             {text: '.txt', correct: false}
         ]
     },
     {
         question: 'What is the process of finding and fixing errors in a program called?',
         answers: [
-            {text: 'Debugging', correct: true},
             {text: 'Compiling', correct: false},
             {text: 'Testing', correct: false},
-            {text: 'Optimizing', correct: false}
+            {text: 'Optimizing', correct: false},
+            {text: 'Debugging', correct: true},
         ]
     },
     {
@@ -121,8 +121,8 @@ const questions = [
     {
         question: 'What does HTML stand for?',
         answers: [
-            {text: 'Hypertext Markup Language', correct: true},
             {text: 'Hyperlink and Text Markup Language', correct: false},
+            {text: 'Hypertext Markup Language', correct: true},
             {text: 'Home Tool Markup Language', correct: false},
             {text: 'Hypertext Modern Language', correct: false}
         ]
@@ -130,19 +130,19 @@ const questions = [
     {
         question: 'Which operator is used to concatenate strings in JavaScript?',
         answers: [
-            {text: '+', correct: true},
             {text: '-', correct: false},
             {text: '*', correct: false},
+            {text: '+', correct: true},
             {text: '/', correct: false}
         ]
     },
     {
         question: 'What is the result of 10 % 3?',
         answers: [
-            {text: '1', correct: true},
             {text: '3', correct: false},
             {text: '0', correct: false},
-            {text: '10', correct: false}
+            {text: '10', correct: false},
+            {text: '1', correct: true},
         ]
     },
     {
@@ -157,8 +157,8 @@ const questions = [
     {
         question: 'What is the result of 5 > 3?',
         answers: [
-            {text: 'true', correct: true},
             {text: 'false', correct: false},
+            {text: 'true', correct: true},
             {text: 'undefined', correct: false},
             {text: 'null', correct: false}
         ]
@@ -166,9 +166,9 @@ const questions = [
     {
         question: 'Which data structure operates on a "last-in, first-out" basis?',
         answers: [
-            {text: 'Stack', correct: true},
             {text: 'Queue', correct: false},
             {text: 'LinkedList', correct: false},
+            {text: 'Stack', correct: true},
             {text: 'Array', correct: false}
         ]
     }
